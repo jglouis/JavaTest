a1447a6a87dbdbc79318c3e87c63ae9f54963502
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2014-09-04 11:52:44 +0200

    changed Node to its own file in order to make it public

--- tree/BinaryTree.java
+++ tree/BinaryTree.java
@@ -197,30 +197,3 @@ public static void main(String[] args) {
 }
 }
 
-class Node {
-
-	int key;
-	String name;
-
-	Node leftChild;
-	Node rightChild;
-
-	Node(int key, String name) {
-
-		this.key = key;
-		this.name = name;
-
-	}
-
-	public String toString() {
-
-		return name + " has the key " + key;
-
-		/*
-		 * return name + " has the key " + key + "\nLeft Child: " + leftChild +
-		 * "\nRight Child: " + rightChild + "\n";
-		 */
-
-	}
-
-}
